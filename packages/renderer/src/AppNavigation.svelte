67ff804a4399feec2ab937853ed11ef565c95b1b
Evzen Gasta <evzen.ml@seznam.cz>
2024-07-30 09:23:45 +0200

    chore: use dropdownmenu component for always on buddy widget popup menu
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/renderer/src/AppNavigation.svelte
+++ packages/renderer/src/AppNavigation.svelte
@@ -1,9 +1,16 @@
 <svelte:options runes={true} />
 
 <script lang="ts">
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
+import { Tooltip } from '@podman-desktop/ui-svelte';
+import { ContainerIcon } from '@podman-desktop/ui-svelte/icons';
+import { onDestroy, onMount } from 'svelte';
+import type { Unsubscriber } from 'svelte/store';
+import Fa from 'svelte-fa';
 import { onMount } from 'svelte';
 import type { TinroRouteMeta } from 'tinro';
 
+import AuthActions from './lib/authentication/AuthActions.svelte';
 import { CommandRegistry } from './lib/CommandRegistry';
 import NewContentOnDashboardBadge from './lib/dashboard/NewContentOnDashboardBadge.svelte';
 import AccountIcon from './lib/images/AccountIcon.svelte';
@@ -12,6 +19,60 @@ import SettingsIcon from './lib/images/SettingsIcon.svelte';
 import NavItem from './lib/ui/NavItem.svelte';
 import NavRegistryEntry from './lib/ui/NavRegistryEntry.svelte';
 import NavSection from './lib/ui/NavSection.svelte';
+import { combinedInstalledExtensions } from './stores/all-installed-extensions';
+import { containersInfos } from './stores/containers';
+import { contributions } from './stores/contribs';
+import { imagesInfos } from './stores/images';
+import { kubernetesContexts } from './stores/kubernetes-contexts';
+import {
+  kubernetesCurrentContextConfigMaps,
+  kubernetesCurrentContextDeployments,
+  kubernetesCurrentContextIngresses,
+  kubernetesCurrentContextNodes,
+  kubernetesCurrentContextPersistentVolumeClaims,
+  kubernetesCurrentContextRoutes,
+  kubernetesCurrentContextSecrets,
+  kubernetesCurrentContextServices,
+} from './stores/kubernetes-contexts-state';
+import { podsInfos } from './stores/pods';
+import { volumeListInfos } from './stores/volumes';
+
+let podInfoSubscribe: Unsubscriber;
+let containerInfoSubscribe: Unsubscriber;
+let imageInfoSubscribe: Unsubscriber;
+let volumeInfoSubscribe: Unsubscriber;
+let contextsSubscribe: Unsubscriber;
+let nodesSubscribe: Unsubscriber;
+let deploymentsSubscribe: Unsubscriber;
+let persistentVolumeClaimsSubscribe: Unsubscriber;
+let servicesSubscribe: Unsubscriber;
+let ingressesSubscribe: Unsubscriber;
+let routesSubscribe: Unsubscriber;
+let configmapsSubscribe: Unsubscriber;
+let secretsSubscribe: Unsubscriber;
+let combinedInstalledExtensionsSubscribe: Unsubscriber;
+
+let podCount = '';
+let containerCount = '';
+let imageCount = '';
+let volumeCount = '';
+let configmapsCount = 0;
+let secretsCount = 0;
+let configmapSecretsCount = '';
+let persistentVolumeClaimsCount = '';
+let contextCount = 0;
+let deploymentCount = '';
+let nodeCount = '';
+let serviceCount = '';
+let ingressesCount = 0;
+let routesCount = 0;
+let ingressesRoutesCount = '';
+let extensionCount = '';
+let authActions: AuthActions;
+let outsideWindow: HTMLDivElement;
+
+const imageUtils = new ImageUtils();
+export let exitSettingsCallback: () => void;
 import { navigationRegistry } from './stores/navigation/navigation-registry';
 
 let { exitSettingsCallback, meta = $bindable() }: { exitSettingsCallback: () => void; meta: TinroRouteMeta } = $props();
@@ -71,13 +132,14 @@ function clickSettings(b: boolean) {
 
   <div class="grow"></div>
 
-  <NavItem
-    href="/accounts"
-    tooltip="Accounts"
-    bind:meta={meta}
-    onClick={event => window.showAccountsMenu(event.x, event.y)}>
-    <AccountIcon size={iconSize} />
-  </NavItem>
+  <div bind:this={outsideWindow}>
+    <NavItem href="/accounts" tooltip="" bind:meta={meta} onClick={event => authActions.onButtonClick(event)}>
+      <Tooltip bottomRight tip="Accounts">
+        <AccountIcon size={iconSize} />
+      </Tooltip>
+      <AuthActions bind:this={authActions} outsideWindow={outsideWindow} />
+    </NavItem>
+  </div>
 
   <NavItem
     href="/preferences"
